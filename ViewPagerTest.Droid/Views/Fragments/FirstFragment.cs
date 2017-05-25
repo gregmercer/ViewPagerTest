cbe58918fcbfcceb1b83c04cd52d766765b1ad43
Greg Mercer (GBear) <gmercer@stanford.edu>
2017-05-25 11:06:10 -0700

    Added Registers for each of the fragments

--- ViewPagerTest.Droid/Views/Fragments/FirstFragment.cs
+++ ViewPagerTest.Droid/Views/Fragments/FirstFragment.cs
@@ -4,9 +4,12 @@ using Android.Runtime;
 using Android.Views;
 using MvvmCross.Droid.Support.V4;
 using MvvmCross.Binding.Droid.BindingContext;
+using MvvmCross.Platform.IoC;
 
 namespace ViewPagerTest.Droid.Views.Fragments
 {
+	[MvxUnconventional]
+	[Register("viewpagertest.droid.views.fragments.FirstFragment")]
     public class FirstFragment : MvxFragment
     {
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
